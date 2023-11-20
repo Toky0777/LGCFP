fd5d6ef3d822388ec0d2eab040fb959e95657c48
Toky0777 <ramananjaratoky07@gmail.com>
2023-11-20 14:53:03 +0300

    merged

--- src/components/MyInput.js
+++ src/components/MyInput.js
@@ -6,7 +6,66 @@ export function MyInput(props) {
   return (
     <div className="">
       <Input label={label} color="purple" />
-      {success ? <Typography
+      {/* {success ? <Typography
+        variant="small"
+        color="green"
+        className="mt-2 flex items-center gap-1 font-normal"
+      >
+        {/* <InfoIcon /> */}
+        {/* Info norma. */}
+      {/* </Typography> :
+
+        <Typography
+          variant="small"
+          color="red"
+          className="mt-2 flex items-center gap-1 font-normal"
+        >
+          <InfoIcon />
+          Message d'erreur.
+        </Typography> */}
+
+    </div>
+  );
+}
+
+export function MyTextarea(props) {
+  const { label, success } = props;
+  return (
+    <div className="">
+      <Textarea label={label} color="purple" />
+      {/* {success ? <Typography
+        variant="small"
+        color="green"
+        className="mt-2 flex items-center gap-1 font-normal"
+      >
+        <InfoIcon />
+        Info norma.
+      </Typography> :
+
+        <Typography
+          variant="small"
+          color="red"
+          className="mt-2 flex items-center gap-1 font-normal"
+        >
+          <InfoIcon />
+          Message d'erreur.
+        </Typography>} */}
+
+    </div>
+  );
+}
+export function MySelectInput(props) {
+  const { label, data, success } = props;
+  return (
+    <div className="">
+      <Select label={label} color="purple">
+        {data.map((item, index) => (
+          <Option key={item}>{item}</Option>
+        ))
+
+        }
+      </Select>
+      {/* {success ? <Typography
         variant="small"
         color="green"
         className="mt-2 flex items-center gap-1 font-normal"
